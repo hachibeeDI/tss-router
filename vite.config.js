61f11726a46ea811fcbc9b03157d8810418ac3ef
Ogura Daiki <8hachibee125@gmail.com>
2025-04-10 16:20:48 +0900

    testing lib

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import {defineConfig} from 'vitest/config';
+
+export default defineConfig({
+  test: {
+    globals: true,
+    // environment: 'happy-dom',  DO NOT USE IT
+    environment: 'jsdom',
+    setupFiles: ['./setup/vite-global-setup.js'],
+  },
+});
