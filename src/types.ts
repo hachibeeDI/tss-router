3a2e48e2d4fdd9e381676cd5213448afae93484e
Ogura Daiki <8hachibee125@gmail.com>
2025-04-11 10:01:48 +0900

    fix dependencies

--- src/types.ts
+++ src/types.ts
@@ -24,8 +24,37 @@ export type PathParser<Path extends string> = Path extends `${infer Uri}?${infer
     }
   : BuildParam<BodyParser<Path>>;
 
-type PathMatcher = {
-  match: (pathname: string) => boolean;
+type Path = {
+  pathname: string;
+  search: string;
+  hash: string;
+};
+
+/**
+ * expect History.Location but you can use any compatible alternatives
+ */
+export type Location = Path & {
+  state?: unknown;
+  // key: string;
+};
+
+type To = string | Partial<Path>;
+
+type HistoryActions = 'PUSH' | 'REPLACE' | 'POP';
+
+/**
+ * Assum remix-run/history, but you can choose any alternative.
+ * Or write your wrapper.
+ *
+ * Looks like remix-run/history has bunch of unnecessary dependencies and is going to be obsolete.
+ * Propbabry I need to make better solution with zero dependency.
+ */
+export type History = {
+  action: HistoryActions;
+  location: Location;
+  push: (to: To, state?: unknown) => void;
+  replace: (to: To, state?: unknown) => void;
+  listen: (listener: (location: {action: HistoryActions; location: Location}) => void) => () => void;
 };
 
 /**
@@ -34,6 +63,7 @@ type PathMatcher = {
 export type Routing<Path extends string> = {
   path: Path;
   match: (pathname: string) => boolean;
-  render: (args: PathParser<Path>) => ReactNode;
+  render: (loc: Location) => ReactNode;
   buildUrl: (args: PathParser<Path>) => string;
+  extractParams: (location: Location) => PathParser<Path>;
 };
